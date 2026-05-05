a8849e772ad728343a16fcf00ee0f922e20b0c41
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2026-05-05 14:10:15 +0500

    feat: updated story book

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(ts|tsx)'],
-  addons: ['@storybook/addon-essentials', 'storybook-addon-pseudo-states'],
+  addons: ['@storybook/addon-docs', 'storybook-addon-pseudo-states'],
   framework: { name: '@storybook/react-vite', options: {} },
   typescript: { reactDocgen: 'react-docgen-typescript' },
   viteFinal: async (cfg) => mergeConfig(cfg, { plugins: [tailwindcss()] }),
